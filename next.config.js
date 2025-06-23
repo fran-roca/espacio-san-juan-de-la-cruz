a89c65f1b50d27519b642c6da1fb6baea3bd922b
Fran <fran@gmail.com>
2025-06-23 17:52:51 +0100

    Deployment Failed. Fixing

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    basePath: "/espacio-san-juan-de-la-cruzespacio-san-juan-de-la-cruz",
+    output: "export",
+    reactStrictMode: true,
+};
+
+module.exports = nextConfig;
